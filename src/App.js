d2475fe1b2ed89626f84dc525c698a39f7e1ea8c
Muhammad Al Amin <85267058+muhammadalaminwd@users.noreply.github.com>
2022-05-23 20:03:16 +0600

    blogs added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Shared/Navbar';
 import Footer from './Shared/Footer';
 import Home from './Home/Home';
 import Purchase from './Purchase/Purchase';
+import Blogs from './Blogs/Blogs';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/purchase' element={<Purchase />} />
+      <Route path='/blogs' element={<Blogs />} />
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Blogs/Blogs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center mb-3 text-accent'>Question Answer</h1>
+
+            <div className='ml-5 mb-4'>
+            <h1 className='text-2xl text-primary'>Q1: How will you improve the performance of a react application?</h1>
+            <h2 className='text-2xl text-primary'>Ans: </h2>
+            </div>
+
+            <div className='ml-5 mb-4'>
+            <h1 className='text-2xl text-primary'>Q2: What are the different ways to manage a state in a React Application?</h1>
+            <h2 className='text-2xl text-primary'>Ans: </h2>
+            </div>
+
+            <div className='ml-5 mb-4'>
+            <h1 className='text-2xl text-primary'>Q3: How does prototypical inheritance work?</h1>
+            <h2 className='text-2xl text-primary'>Ans: </h2>
+            </div>
+
+            <div className='ml-5 mb-4'>
+            <h1 className='text-2xl text-primary'>Q4: You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h1>
+            <h2 className='text-2xl text-primary'>Ans: </h2>
+            </div>
+
+            <div className='ml-5 mb-4'>
+            <h1 className='text-2xl text-primary'>Q5: What is a unit test? Why should write unit tests?</h1>
+            <h2 className='text-2xl text-primary'>Ans: </h2>
+            </div>
+
+        </div>
+    );
+};
+
+export default Blogs;
